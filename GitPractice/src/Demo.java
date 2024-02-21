964f8340d63d0416d87e582fc3d857f7943bea0a
Manu555 <manojtn.4545@gmail.com>
2024-02-21 12:27:41 +0530

    commit update

--- GitPractice/src/Demo.java
+++ GitPractice/src/Demo.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				System.out.println("Hello Gir i am new one");
+		System.out.println("Hello Gir i am new one");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
 	}
-
 }
